e4a89b2cde57127b8dbd74bc512ddcf26d514338
Maxime <ewiie2503@gmail.com>
2022-05-05 17:44:40 +0200

    update

---
+++ ConnectionBDD/connection.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Connection</title>
+    <meta charset="utf-8">
+</head>
+    <body>
+        <?php
+            $serveur = "51.77.211.205";
+            $login = "admin";
+            $mdp = "dev1";
+
+            try{
+                $bdd = new PDO("mysql:host=$serveur;dbname=LOC_MNS", $login, $mdp);
+                $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $insertion = "INSERT INTO utilisateurs(nom, prenom, email) 
+                            VALUES(:Dupond, :Albert, :'dupond.albert@test.com')";
+
+                $connexion->exec($insertion);
+                echo "Insertion réussie";
+            }
+            catch(PDOException $e){
+                echo "Erreur : " . $e->getMessage();
+            }
+        ?>
+    </body>
+</html>
---
+++ Formulaire/formulaire.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page de traitement</title>
+    <meta charset="utf-8">
+</head>
+
+    <body>
+        <form method="POST" action="target.php">
+            <p>
+                <label for="nom">Entrez votre prénom</label>
+                <input type="text" name="nom" id="nom"
+            </p>
+            <p>
+                <label for="prénom">Entrez votre prénom</label>
+                <input type="text" name="prénom" id="prénom"
+            </p>
+            <p>
+                <label for="email">Entrez votre email</label>
+                <input type="text" name="email" id="email"
+            </p>
+            <p>
+                <input type="submit" value="Envoyer">
+            </p>
+        </form>
+    </body>
+</html>
