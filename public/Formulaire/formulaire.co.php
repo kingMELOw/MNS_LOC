3d3505bb030b94a36a0a1c9a36901a636396b3c3
Félix Scaron <93914675+kingMELOw@users.noreply.github.com>
2022-06-03 16:26:25 +0200

    CONNEXIOOOOON

--- public/Formulaire/formulaire.co.php
+++ public/Formulaire/formulaire.co.php
@@ -14,17 +14,19 @@
     <form method="POST" action="target.php">
         <table>
             <tr>
-                <td><label for="Login">Login</label></td>
-                <td><input type="text" name="Login" id="Login" /></td>
+                <div data-validate="Ce champ est obligatoire">
+                    <td><label for="login">Login</label></td>
+                    <td><input type="text" name="login" placeholder="Nom d'utilisateur"/></td>
+                </div>
             </tr>
             <tr>
-                <td><label for="mdp">Password</label></td>
-                <td><input type="password" name="mdp" id="mdp" />
+                <div data-validate="Ce champ est obligatoire">
+                    <td><label for="mdp">Password</label></td>
+                    <td><input type="password" name="mdp" placeholder="Mot de passe"/>
+                </div>
             </tr>
         </table>
-        <div id="test">
             <input type="submit" value="Connexion" id="btnEnvoyer">
-        </div>
     </form>
     <div><p>Pas inscrit ? Faites une demande <a href="../Inscription.html">d'inscription</a></p></div>
     <a href="../index.html">
